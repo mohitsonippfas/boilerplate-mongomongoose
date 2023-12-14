f8f97c85a043a3900668a25efb4e8bb1b6a47e79
mohitsonippfas <143504988+mohitsonippfas@users.noreply.github.com>
2023-12-14 07:26:01 +0000

    schema creation

--- myApp.js
+++ myApp.js
@@ -5,7 +5,17 @@ mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Schema=mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+
 let Person;
+ Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
